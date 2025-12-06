8cb95505dfd762eedd8f1fa73dff01a347562d4a
achernecova <achernecova1904@gmail.com>
2025-12-07 02:19:57 +0300

    Изменила локатор для карточек скролла

--- pages/main_page_selene.py
+++ pages/main_page_selene.py
@@ -40,9 +40,9 @@ class MainPageSelene:
 
     @allure.step("Открываем страницу из блока 'Website Packages'")
     def open_page_from_website_packages_block_by_index(self, value):
-        self.scroll_element.scroll_to_element("(//*[@class='team-card'])", value)
+        self.scroll_element.scroll_to_element("(//*[@class='team-card']//a)", value)
         self.cookie_modal.close_modal()
-        self.scroll_element.scroll_to_element("(//*[@class='team-card'])", value)
+        self.scroll_element.scroll_to_element("(//*[@class='team-card']//a)", value)
         more_buttons = self.block_website_packages_more.button_website_packages_more()
         more_buttons[value - 1].click()
 
