d7c6fac565449edeb730ac82247e8859e6d6b3fe
achernecova <achernecova1904@gmail.com>
2025-12-07 16:15:22 +0300

    скрыла в close_modal клик по свободной области. Проверяем тесты на открытие страниц

--- page_elements/cookies_popup.py
+++ page_elements/cookies_popup.py
@@ -1,9 +1,11 @@
 from selene import be, browser
+from selenium.webdriver.common.by import By
 
 
 class CookiesPopup:
 
     @staticmethod
     def close_modal():
+        browser.element((By.TAG_NAME, "body")).click()
         element_close_modal = browser.element(".close-modal").should(be.visible)
         element_close_modal.click()
